a219c0667ef0a2daa85667b022947d083f261e13
cxronen <ronen.riesenfeld@checkmarx.com>
2020-05-28 12:50:55 -0500

    comment

--- App_Code/temp/New Folder/CCUtility.cs
+++ App_Code/temp/New Folder/CCUtility.cs
@@ -113,7 +113,7 @@ login(password );
 
 		public static string GetValue(DataRow row, string field) {
 			if (row[field].ToString() == null)
-				return ""; //123
+				return ""; //123456
 			else
 				return row[field].ToString();
 		}
