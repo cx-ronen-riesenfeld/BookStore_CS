22c1f1c83c0bf68be74402d0098302a5fe8e2beb
Ronen Riesenfeld <ronen.riesenfeld@checkmarx.com>
2020-06-22 10:19:33 -0500

    test123

--- AdminMenu.cs
+++ AdminMenu.cs
@@ -5,7 +5,7 @@ namespace Book_Store
 //
 //    Filename: AdminMenu.cs
 //    Generated with CodeCharge 2.0.5
-//    ASP.NET C#.ccp build 03/07/2002  aaa
+//    ASP.NET C#.ccp build 03/07/2002  aaabbb
 //
 //-------------------------------
 //
